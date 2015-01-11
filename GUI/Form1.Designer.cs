ccc8c50d9431a48d3e9b7eafb87365d59ba9f292
Denis Chusovitin <Chusovitin.Den@gmail.com>
2015-01-11 22:30:58 +0400

    Fix GUI

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -38,7 +38,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "button1";
+            this.button1.Text = "Press me";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -47,9 +47,8 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(83, 336);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 3;
-            this.label1.Text = "label1";
             // 
             // Form1
             // 
@@ -58,8 +57,9 @@
             this.ClientSize = new System.Drawing.Size(622, 358);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Quadrilaterals";
             this.Load += new System.EventHandler(this.Form1_Load_1);
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.ResumeLayout(false);
