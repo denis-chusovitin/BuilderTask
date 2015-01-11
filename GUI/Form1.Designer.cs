9f2996964d71608a1ce88822f084c3ae344e6d11
Denis Chusovitin <Chusovitin.Den@gmail.com>
2015-01-11 23:03:25 +0400

    Fix form

--- GUI/Form1.Designer.cs
+++ GUI/Form1.Designer.cs
@@ -58,6 +58,8 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Quadrilaterals";
             this.Load += new System.EventHandler(this.Form1_Load_1);
